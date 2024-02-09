421c30ecfcd944171d9005496d62be69f4701dda
OliverJoo <jjaemoon@nate.com>
2024-02-09 11:01:50 +0900

    feat: add mapper between model & local db

---
+++ lib/us_stock_app/data/mapper/company_mapper.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_class/us_stock_app/data/data_source/local/company_listing_entity.dart';
+
+import '../../domain/model/company_listing.dart';
+
+extension ToCompanyListing on CompanyListingEntity {
+  CompanyListing toCompanyListing() {
+    return CompanyListing(
+      symbol: symbol,
+      name: name,
+      exchange: exchange,
+    );
+  }
+}
+
+extension ToCompanyListingEntity on CompanyListing {
+  CompanyListingEntity toCompanyListingEntity() {
+    return CompanyListingEntity(
+      symbol: symbol,
+      name: name,
+      exchange: exchange,
+    );
+  }
+}
