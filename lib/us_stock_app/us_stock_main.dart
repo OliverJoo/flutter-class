cdaa439528943a7be58bf6be2d70bd35c01e73d5
OliverJoo <jjaemoon@nate.com>
2024-02-09 10:47:01 +0900

    refactor: refactor folder structure

--- lib/us_stock_app/us_stock_main.dart
+++ lib/us_stock_app/us_stock_main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_class/us_stock_app/color_schemes.g.dart';
+import 'package:flutter_class/us_stock_app/util/color_schemes.dart';
 
 void main() {
   runApp(const MyApp());
