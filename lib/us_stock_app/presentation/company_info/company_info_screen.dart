4fc3bb9004d1a22e4db1ba8759a89e139977c490
OliverJoo <jjaemoon@nate.com>
2024-02-10 13:02:59 +0900

    feat: add company info model, dto, screen, state, viewModel, viewModelTest

---
+++ lib/us_stock_app/presentation/company_info/company_info_screen.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_class/us_stock_app/domain/model/company_info.dart';
+import 'package:flutter_class/us_stock_app/presentation/company_info/company_info_view_model.dart';
+import 'package:flutter_class/us_stock_app/presentation/company_info/components/stock_chart.dart';
+import 'package:provider/provider.dart';
+
+import 'company_info_state.dart';
+
+class CompanyInfoScreen extends StatelessWidget {
+  const CompanyInfoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final viewModel = context.watch<CompanyInfoViewModel>();
+    final state = viewModel.state;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            if (state.errorMessage != null)
+              Center(child: Text(state.errorMessage!)),
+            if (state.isLoading)
+              const Center(child: CircularProgressIndicator()),
+            if (state.isLoading == false && state.errorMessage == null)
+              _buildBody(context, state),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBody(BuildContext context, CompanyInfoState state) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            state.companyInfo!.name,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+          Text(
+            state.companyInfo!.symbol,
+            style: const TextStyle(
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          const Divider(),
+          Text(
+            'Industry : ${state.companyInfo!.industry}',
+            style: const TextStyle(
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+          Text(
+            'Country : ${state.companyInfo!.country}',
+            style: const TextStyle(
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+          const Divider(),
+          Text(
+            state.companyInfo!.description,
+            style: const TextStyle(fontSize: 12),
+          ),
+          const SizedBox(height: 16),
+          const Text(
+            'Stock Graph',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 16),
+          if (state.stockInfos.isNotEmpty)
+            StockChart(
+              infos: state.stockInfos,
+              graphColor: Theme.of(context).colorScheme.primary,
+              textColor: Theme.of(context).colorScheme.onSurface,
+            ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/us_stock_app/presentation/company_info/company_info_state.dart
@@ -0,0 +1,22 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+import '../../domain/model/company_info.dart';
+import '../../domain/model/intraday_info.dart';
+
+part 'company_info_state.freezed.dart';
+
+part 'company_info_state.g.dart';
+
+@freezed
+class CompanyInfoState with _$CompanyInfoState {
+  const factory CompanyInfoState({
+    CompanyInfo? companyInfo,
+    @Default(false) bool isLoading,
+    String? errorMessage,
+    @Default([]) List<IntradayInfo> stockInfos,
+  }) = _CompanyInfoState;
+
+  factory CompanyInfoState.fromJson(Map<String, Object?> json) =>
+      _$CompanyInfoStateFromJson(json);
+}
